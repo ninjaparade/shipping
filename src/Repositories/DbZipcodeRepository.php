2804d1caeb96940c1cc51573d240b5047b94287f
Ninjaparade <ninja@ninjaparade.ca>
2014-07-23 20:48:19 -0700

    cleanup
    Signed-off-by: Ninjaparade <ninja@ninjaparade.ca>

--- src/Repositories/DbZipcodeRepository.php
+++ src/Repositories/DbZipcodeRepository.php
@@ -179,12 +179,12 @@ class DbZipcodeRepository implements ZipcodeRepositoryInterface {
         {
         	
         	$first = $this->config['express_shipping_first_item'];
-        	$next = $this->config['express_shipping_per_item'];
+        	$next  = $this->config['express_shipping_per_item'];
         	
         }else{
 
         	$first = $this->config['regular_shipping_first_item'];
-        	$next = $this->config['regular_shipping_per_item'];
+        	$next  = $this->config['regular_shipping_per_item'];
             
         }
 
