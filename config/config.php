f5f3c5b83d7c1e343f6eaea95af317c159c1bcc7
Ninjaparade <ninja@ninjaparade.ca>
2014-07-23 20:38:25 -0700

    added shipping options to config
    Signed-off-by: Ninjaparade <ninja@ninjaparade.ca>

--- config/config.php
+++ config/config.php
@@ -12,13 +12,21 @@ return [
 	*/
 
 	'local_pickup'  => '0',
+	'local_pickup_text' => 'Free local pickup',
+
 
 	'local_shipping' => '5',
+
+	'local_shipping_text' => 'Local delivery',
 	
+	'regular_shipping_text' => 'Regular Postage (5-7 day)',
+
 	'regular_shipping_first_item' => '8',
 
 	'regular_shipping_per_item' => '4',
 
+	'express_shipping_text' => 'Priority Postage (1-2 Day)',
+	
 	'express_shipping_first_item' => '16',
 
 	'express_shipping_per_item' => '8'
